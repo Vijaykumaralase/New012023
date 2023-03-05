8012158bb8a791a8c6a2494e0a9ea0d88cef4343
Vijaykumar <vijaykumaralase@gmail.com>
2023-03-05 11:52:42 +0530

    2023 brushuo 01

---
+++ src/main/java/Version3/AutoitPractice/App.java
@@ -0,0 +1,13 @@
+package Version3.AutoitPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
